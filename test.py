192a5e295bfe43bbff35cba6be3a288867a5c4ca
Nikita-git666 <135269505+Nikita-git666@users.noreply.github.com>
2024-04-22 10:20:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import argparse
 
-def interface():
+def start_interface():
 	print ("-----------------------")
-	print ("|       GoB         |")
-	print ("|                       |")
+	print ("|       GoB           |")
+	print ("|                     |")
 	print ("-----------------------")
+def main():
+	parser_start = subparsers.add_parser('start')
