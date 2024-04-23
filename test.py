0108ff59ce221d210b09a3fb15a485dbd27edbca
Nikita-git666 <135269505+Nikita-git666@users.noreply.github.com>
2024-04-23 09:33:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,11 @@ def main():
         parser_start.set_defaults(command='start')
 	
         args = parser.parse_args()
+	if hasattr(args, 'command'):
+        if args.command == 'start':
+            start_interface()
+    else
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
