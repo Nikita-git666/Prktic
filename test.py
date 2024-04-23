380d63cac32f82dda01fd75471e5b8b6117b31e2
Nikita-git666 <135269505+Nikita-git666@users.noreply.github.com>
2024-04-23 09:37:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,6 @@ def start_interface():
 	print("hello")
 def main():
 	parser_start = subparsers.add_parser('start')
-	
-        args = parser.parse_args()
 	if hasattr(args, 'command'):
         if args.command == 'start':
             start_interface()
