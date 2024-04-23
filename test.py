dbee5bcdf05f79c0581a7d81e880402d97811a01
Nikita-git666 <135269505+Nikita-git666@users.noreply.github.com>
2024-04-23 09:40:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import argparse
 
 def start_interface():
-	print("hello")
+    print("Запуск интерфейса..")
 def main():
-	parser_start = subparsers.add_parser('start')
-	if hasattr(args, 'command'):
-        
+    parser = argparse.ArgumentParser(description='Завтра может будет (:')
+    subparsers = parser.add_subparsers(help='Доступные подкоманды')
+
+    # Подкоманда для интерфейса
+    parser_start = subparsers.add_parser('start', help='Запуск интерфейса')
+    parser_start.set_defaults(command='start')
+
+    args = parser.parse_args()
+
+    if hasattr(args, 'command'):
+        if args.command == 'start':
             start_interface()
+    else
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
